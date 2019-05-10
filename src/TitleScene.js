8e021fe5fff591dda2d890447b5e1046eb8c7e3e
t2bs <t2bs_web3@yahoo.co.jp>
2019-05-10 17:15:45 +0900

    更新

--- src/TitleScene.js
+++ src/TitleScene.js
@@ -1,3 +1,5 @@
+var g_runTitleAnime = false;
+
 var TitleScene = cc.Scene.extend({
 	onEnter:function () {
 		this._super();
@@ -8,62 +10,174 @@ var TitleScene = cc.Scene.extend({
 
 //魔方陣
 var MagicCircuitNode = cc.Node.extend({
+	_sprite: null,
 	ctor:function () {
 		this._super();
-		var sprite = cc.Sprite.create(res.img_titleImg02);
-		sprite.setScale(0);
-		this.addChild(sprite);
-		
+		cc.log("**** ctor: MagicCircuitNode  ****");
+		this._sprite = cc.Sprite.create(res.img_titleImg02);
+		this._sprite.setScale(0);
+		this.addChild(this._sprite);
+	},
+	onEnter: function() {
+		this._super();
+		cc.log("**** onEnter: MagicCircuitNode  ****");
+		if(g_runTitleAnime === true){
+			this._sprite.setScale(1.0);
+			this._sprite.runAction(
+						cc.repeat(cc.rotateBy(5.0, 360),Math.pow(2, 30))
+					);
+			return;
+		}
+	},
+	onEnterTransitionDidFinish: function() {
+		this._super();
+		cc.log("**** onEnterTransitionDidFinish: MagicCircuitNode  ****");
+		if(g_runTitleAnime === true){
+			return;
+		}
 		//アニメーション
 		var act = cc.spawn(
 			cc.rotateBy(5.0, 360),
 			cc.scaleTo(0.5, 1.0).easing(cc.easeBackOut())
 		);
-		var repeat = cc.repeat(act,Math.pow(2, 30))
-		sprite.runAction(repeat);
+		var repeat = cc.repeat(act,Math.pow(2, 30));
+		this._sprite.runAction(repeat);
 	}
 });
 
 var TitleLayer = cc.Layer.extend({
+	_winSize: null,
+	_winSizeCenterW: null,
+	_winSizeCenterH: null,
+	_bg: null,
+	_title: null,
+	_chara: null,
+	_magic: null,
+	_btnStart: null,
 	ctor:function () {
 		this._super();
-		var winSize = cc.director.getWinSize();
-		var winSizeCenterW = winSize.width / 2.0;
-		var winSizeCenterH = winSize.height / 2.0;
+		cc.log("**** ctor: TitleLayer  ****");
+		this._winSize = cc.director.getWinSize();
+		this._winSizeCenterW = this._winSize.width / 2.0;
+		this._winSizeCenterH = this._winSize.height / 2.0;
 		
 		//背景の配置
-		var bg = cc.Sprite.create(res.img_bgTitle);
-		bg.setPosition(cc.p(winSizeCenterW, winSizeCenterH));
-		this.addChild(bg, 0);
+		this._bg = cc.Sprite.create(res.img_bgTitle);
+		this._bg.setPosition(cc.p(this._winSizeCenterW, this._winSizeCenterH));
+		this.addChild(this._bg, 0);
 		
 		//タイトルの配置
-		var title = cc.Sprite.create(res.img_titleLogo);
-		title.setPosition(cc.p(winSizeCenterW, winSize.height - 300));
-		title.setScale(0);
-		this.addChild(title, 1);
+		this._title = cc.Sprite.create(res.img_titleLogo);
+		this._title.setPosition(cc.p(this._winSizeCenterW, this._winSize.height - 300));
+		this._title.setScale(0);
+		this.addChild(this._title, 1);
 		
 		//キャラクター配置
-		var chara = cc.Sprite.create(res.img_titleImg01);
-		chara.setPosition(cc.p(winSizeCenterW-50, winSizeCenterH-30));
-		chara.setOpacity(0);
-		this.addChild(chara, 2);
+		this._chara = cc.Sprite.create(res.img_titleImg01);
+		this._chara.setPosition(cc.p(this._winSizeCenterW - 50, this._winSizeCenterH - 30));
+		this._chara.setOpacity(0);
+		this.addChild(this._chara, 2);
 		
 		//魔法陣配置
-		var magic = new MagicCircuitNode();
-		magic.setPosition(cc.p(winSizeCenterW, winSizeCenterH-300));
-		magic.setScale(1.0, 0.5);
-		this.addChild(magic, 1);
+		this._magic = new MagicCircuitNode();
+		this._magic.setPosition(cc.p(this._winSizeCenterW, this._winSizeCenterH - 300));
+		this._magic.setScale(1.0, 0.5);
+		this.addChild(this._magic, 1);
 		
 		//スタートボタンの配置
-		var button = ccui.Button.create();
-		button.setTouchEnabled(false);
-		button.loadTextures(res.img_btnStart, res.img_btnStartOn, null);
-		button.setPosition(cc.p(winSizeCenterW, 200));
-		button.setOpacity(0);
-		this.addChild(button, 2);
+		this._btnStart = ccui.Button.create();
+		this._btnStart.setTouchEnabled(false);
+		this._btnStart.loadTextures(res.img_btnStart, res.img_btnStartOn, null);
+		this._btnStart.setPosition(cc.p(this._winSizeCenterW, 200));
+		this._btnStart.setOpacity(0);
+		this.addChild(this._btnStart, 2);
+		
+		//スタートボタンのタッチイベントを設定
+		this._btnStart.addTouchEventListener(function(sender, type){
+			switch (type) {
+			case ccui.Widget.TOUCH_BEGAN: // ボタンにタッチした時
+				break;
+			case ccui.Widget.TOUCH_MOVED: // ボタンにタッチ中
+				break;
+			case ccui.Widget.TOUCH_ENDED: // ボタンを離した時
+				this.onStart();
+				break;
+			case ccui.Widget.TOUCH_CANCELED: // キャンセルした時
+				break;
+			}
+		}, this);
+	},
+	onEnter: function() {
+		this._super();
+		cc.log("**** onEnter: TitleLayer  ****");
+		if(g_runTitleAnime === true){
+			this._chara.runAction(
+				cc.sequence(
+					cc.spawn(
+						cc.moveBy(0, cc.p(0, 30)),
+						cc.fadeIn(0)
+					),
+					cc.repeat(
+						cc.sequence(
+							cc.moveBy(1.0, cc.p(0, -30)).easing(cc.easeSineIn()),
+							cc.delayTime(0.3),
+							cc.moveBy(1.0, cc.p(0, 30)).easing(cc.easeSineIn()),
+						),Math.pow(2, 30)
+					)
+				)
+			);
+			
+			var act = cc.repeat( 
+							cc.sequence(
+								cc.rotateTo(0.05, 10).easing(cc.easeIn(3)),
+								cc.rotateTo(0.05, -10).easing(cc.easeIn(3))
+						), 10);
+			this._title.runAction(
+				cc.sequence(
+					cc.scaleTo(0, 1.0),
+					cc.delayTime(2.0),
+					cc.repeat(
+						cc.sequence(
+							cc.rotateBy(0.5, 720).easing(cc.easeIn(3)),
+							cc.delayTime(1.0),
+							cc.spawn(
+								act,
+								cc.scaleTo(0.5, 2.0).easing(cc.easeIn(3)),
+								cc.fadeOut(0.5).easing(cc.easeIn(3))
+							),
+							cc.delayTime(1.0),
+							cc.rotateTo(0, 0),
+							cc.spawn(
+								cc.scaleTo(0.3, 1.0).easing(cc.easeIn(3)),
+								cc.fadeIn(0.3).easing(cc.easeIn(3))
+							),
+							cc.delayTime(1.0)
+						),Math.pow(2, 30)
+					)
+				)
+			);
+			
+			this._btnStart.setTouchEnabled(true);
+			this._btnStart.runAction(
+				cc.sequence(
+					cc.fadeIn(0)
+				)
+			);
+			return;
+		}
+	},
+	//トランジション終わり時
+	onEnterTransitionDidFinish: function() {
+		this._super();
+		cc.log("**** onEnterTransitionDidFinish: TitleLayer  ****");
+		if(g_runTitleAnime === true){
+			return;
+		}
+		
+		g_runTitleAnime = true;
 		
 		//キャラクターのアニメーション
-		chara.runAction(
+		this._chara.runAction(
 			cc.sequence(
 				cc.delayTime(1.0),
 				cc.spawn(
@@ -91,7 +205,7 @@ var TitleLayer = cc.Layer.extend({
 							cc.rotateTo(0.05, 10).easing(cc.easeIn(3)),
 							cc.rotateTo(0.05, -10).easing(cc.easeIn(3))
 					), 10);
-		title.runAction(
+		this._title.runAction(
 			cc.sequence(
 				cc.delayTime(3.0),
 				cc.scaleTo(0.3, 1.0).easing(cc.easeBackOut()),
@@ -118,41 +232,28 @@ var TitleLayer = cc.Layer.extend({
 		);
 		
 		//ボタンのアニメーション
-		button.runAction(
+		var btnStart = this._btnStart;
+		this._btnStart.runAction(
 			cc.sequence(
 				cc.delayTime(3.5),
 				cc.fadeIn(0.3).easing(cc.easeOut(3)),
 				cc.callFunc(function(){
-					button.setTouchEnabled(true);
+					btnStart.setTouchEnabled(true);
 				})
 			)
 		);
-		
-		//ボタンのタッチイベントを設定
-		button.addTouchEventListener(function(sender, type){
-			switch (type) {
-			case ccui.Widget.TOUCH_BEGAN: // ボタンにタッチした時
-				break;
-			case ccui.Widget.TOUCH_MOVED: // ボタンにタッチ中
-				break;
-			case ccui.Widget.TOUCH_ENDED: // ボタンを離した時
-				this.onBtnStart();
-				break;
-			case ccui.Widget.TOUCH_CANCELED: // キャンセルした時
-				break;
-			}
-		}, this);
 	},
 	//スタート処理
-	onBtnStart: function(){
+	onStart: function(){
+		//わらわら出現
 		this.schedule(this.spawnChara, 0.05, 20);
 		cc.director.runScene(cc.TransitionFade.create(3, new GameScene()));
 	},
+	//ランダムにキャラクターを配置
 	spawnChara: function(){
-		var winSize = cc.director.getWinSize();
 		var sprite = cc.Sprite.create(res.img_titleImg01);
-		var x = Math.floor(Math.random() * winSize.width);
-		var y = Math.floor(Math.random() * winSize.height);
+		var x = Math.floor(Math.random() * this._winSize.width);
+		var y = Math.floor(Math.random() * this._winSize.height);
 		sprite.setPosition(cc.p(x, y));
 		sprite.setOpacity(0);
 		sprite.setScale(0);
@@ -166,5 +267,5 @@ var TitleLayer = cc.Layer.extend({
 						cc.delayTime(1.0)
 					);
 		sprite.runAction(act);
-	},
+	}
 });
