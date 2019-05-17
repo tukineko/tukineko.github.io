1b00e993cb4f8ede3f7c57e3bd993f7681595f17
t2bs <t2bs_web3@yahoo.co.jp>
2019-05-17 17:26:30 +0900

    更新

--- src/TitleScene.js
+++ src/TitleScene.js
@@ -1,3 +1,34 @@
+var GridLayer = cc.Layer.extend({
+	ctor:function () {
+		this._super();
+		var winSize = cc.director.getWinSize();
+
+		// 線の太さと色
+		var lineSize = 1;
+		var lineW = 40;
+		var lineH = 40;
+		var color = cc.color(0, 0, 125);
+
+		// 縦線を引く
+		for (var x = 1; x <= (winSize.width / lineW); x++)
+		{
+			var xPoint = x * lineW;
+			var draw = new cc.DrawNode();
+			draw.setPosition(cc.p(0, 0));
+			draw.drawSegment(cc.p(xPoint, 0), cc.p(xPoint, winSize.height), lineSize, color);
+			this.addChild(draw, 999);
+		}
+		// 横線を引く
+		/*for (var y = 1; y &lt; pTiledMap->getMapSize().height; y++)
+		{
+			float yPoint = y * pTiledMap->getTileSize().height;
+			draw->drawSegment(Point(0, yPoint), Point(pTiledMap->getContentSize().width, yPoint), lineSize, color);
+		}*/
+		
+	}
+});
+
+
 //タイトルアニメーションフラグ
 var g_runTitleAnime = false;
 
@@ -6,6 +37,9 @@ var TitleScene = cc.Scene.extend({
 		this._super();
 		var layer = new TitleLayer();
 		this.addChild(layer);
+		
+		var layer2 = new GridLayer();
+		this.addChild(layer2);
 	}
 });
 
